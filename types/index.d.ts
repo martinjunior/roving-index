397d08dc91b6f6c28a8546bce8d3568ae681c53b
Martin Duran <mduran@nerdery.com>
2017-06-16 10:22:31 -0500

    Fixes typing error

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,21 +1,25 @@
 interface IRovingIndexOptions {
-    cyclic: boolean;
-    index: number;
-    pausable: boolean;
-    total: number;
+    cyclic: boolean;
+    index: number;
+    pausable: boolean;
+    total: number;
 }
 
-export declare default class RovingIndex {
-    constructor(config: IRovingIndexOptions);
-    resetIndex(): void;
-    getIndex(): void;
-    getTotal(): number;
-    prev(callback: () => void): void;
-    next(callback: () => void): void;
-    setIndex(index: number, total: number): void;
-    setTotal(total: number): void;
-    static validateIndex(index: number, total: number, options: IRovingIndexOptions): number;
-    static validateFiniteIndex(index: number, total: number): number;
-    static validateCyclicIndex(index: number, total: number): number;
-    static validatePausableIndex(index: number, total: number): number;
+interface IRovingIndexCallback {
+    (oldIndex: number, newIndex: number): void;
+}
+
+export default class RovingIndex {
+    constructor(config: IRovingIndexOptions);
+    resetIndex(): void;
+    getIndex(): void;
+    getTotal(): number;
+    prev(callback: IRovingIndexCallback): void;
+    next(callback: IRovingIndexCallback): void;
+    setIndex(index: number, total: number): void;
+    setTotal(total: number): void;
+    static validateIndex(index: number, total: number, options: IRovingIndexOptions): number;
+    static validateFiniteIndex(index: number, total: number): number;
+    static validateCyclicIndex(index: number, total: number): number;
+    static validatePausableIndex(index: number, total: number): number;
 }
